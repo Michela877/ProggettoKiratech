14120b49f9851e9e65b5876256c64b81089b7a53
Michela877 <90michela90@gmail.com>
2024-11-20 23:59:26 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,15 @@
-provider "virtualbox" {}
+terraform {
+  required_providers {
+    hyperv = {
+      source  = "hashicorp/hyperv"
+      version = "1.0.0"  # Assicurati di usare una versione compatibile
+    }
+  }
+}
+
+provider "hyperv" {
+  host = "localhost"  # Indica l'host di Hyper-V, "localhost" se è sulla stessa macchina
+}
 
 variable "vm_details" {
   description = "Details of the VMs to create"
@@ -16,34 +27,36 @@ variable "vm_details" {
   ]
 }
 
-resource "virtualbox_vm" "vms" {
+resource "hyperv_virtual_machine" "vms" {
   for_each = { for vm in var.vm_details : vm.name => vm }
 
-  name   = each.value.hostname
-  cpus   = each.value.cpu
-  memory = each.value.memory
-  image  = "C:/Users/User/Downloads/CentOS-Stream-9-latest-x86_64-dvd1.iso" # Cambia con il percorso corretto della tua ISO
+  name      = each.value.hostname
+  cpu_count = each.value.cpu
+  memory_startup_bytes = each.value.memory * 1024 * 1024  # Converti MB in byte
 
-  # Storage Controller
-  storage_controller {
-    name  = "SATA Controller"
-    type  = "sata"
-  }
-
-  # Disks
+  # Configurazione del disco
   disk {
-    name = "${each.value.name}-disk.vdi"
-    size = 20480 # 20 GiB
+    name            = "${each.value.name}-disk.vhdx"
+    size            = 20 # GB
+    type            = "dynamic"  # Tipo di disco dinamico che si espande quando viene scritto
   }
 
-  # Network
+  # Configurazione della rete
   network_adapter {
-    type           = "Scheda con bridge"
-    bridge_adapter = "Realtek Gaming 2.5GbE Family Controller" # Cambia con il nome corretto della scheda bridge
+    switch_name = "Default Switch"  # Cambia con il nome del tuo switch virtuale
+    static_mac_address = "00-15-5D-00-00-01"  # (opzionale) MAC address statico
   }
+
+  # Configurazione del CD-ROM per l'ISO
+  dvd_drive {
+    path = "C:/Users/User/Downloads/CentOS-Stream-9-latest-x86_64-dvd1.iso"  # Cambia con il percorso dell'ISO
+  }
+
+  # Aggiungi eventuali altre configurazioni come necessarie
 }
 
 output "vm_ips" {
   description = "IP addresses of the VMs"
-  value = { for vm in virtualbox_vm.vms : vm.name => vm.ipv4_address }
+  value = { for vm in hyperv_virtual_machine.vms : vm.name => vm.ip_address }
 }
+
